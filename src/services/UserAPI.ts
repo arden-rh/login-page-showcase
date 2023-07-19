1b185bf79c189287be055f49eeaf68986b1dcfdb
Arden <arden.haldorson@gmail.com>
2023-07-19 20:55:30 +0200

    setting up the json-server functions with axios

---
+++ src/services/UserAPI.ts
@@ -0,0 +1,19 @@
+import axios from 'axios'
+import { User, loggedInUser } from '../types'
+
+const BASE_URL = "http://localhost:3000"
+
+/**
+ * GET users request
+ */
+export const getUsers = async () => {
+	const response = await axios.get(`${BASE_URL}/users`)
+	return response.data as User[]
+}
+
+/** POST user login request */
+export const loginUser = async (data: User) => {
+	const response = await axios.post(`${BASE_URL}/login`, data)
+	return response.data as loggedInUser
+}
+
