f1f7287523f97ad37fa1ddd595ab86086b8ad62b
Arden <arden.haldorson@gmail.com>
2023-07-19 21:05:10 +0200

    added ProtectedRoute, loggedIn state

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,39 @@
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
+import React, { useState } from 'react'
 import Container from 'react-bootstrap/Container'
-import NotFound from './pages/NotFound'
+
+/** Pages */
 import HomePage from './pages/HomePage'
+import NotFound from './pages/NotFound'
 import StarshipsPage from './pages/StarshipsPage'
+
+import ProtectedRoute  from './components/ProtectedRoute'
+
 import './assets/scss/App.scss'
 
 const App = () => {
+
+	const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+	const setLoggedIn = () => {
+		setIsLoggedIn(true)
+	}
 
 	return (
 		<div id="App">
 			<Container className="py-3">
 				<Routes>
-					<Route path='/' element={<HomePage />} />
-					<Route path='/starships' element={<StarshipsPage />} />
+					<Route path='/' element={<HomePage loggedIn={setLoggedIn}/>} />
+					<Route path='/starships' element={
+						<ProtectedRoute isLoggedIn={isLoggedIn}>
+							<StarshipsPage />
+						</ProtectedRoute>} />
 					<Route path='*' element={<NotFound />} />
 				</Routes>
 			</Container>
 
-			<ReactQueryDevtools position='bottom-right'/>
+			<ReactQueryDevtools position='bottom-right' />
 		</div>
 	)
 }
