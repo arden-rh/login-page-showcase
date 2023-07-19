0c95b2f7713bd9da2ec9f977780783456f0dd90f
Arden <arden.haldorson@gmail.com>
2023-07-19 20:51:05 +0200

    fixed a return to Home Page button

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,8 +1,26 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+import { Link, useNavigate } from 'react-router-dom'
+
 const NotFound = () => {
+
+	const navigate = useNavigate()
+
 	return (
-		<div>
-			<h1>Not Found</h1>
-		</div>
+		<>
+			<div>
+				<h1>Not Found</h1>
+			</div>
+			<Link
+				className='return-btn'
+				onClick={(e) => {
+					e.preventDefault();
+					navigate(`/`);
+				}}
+				to={`/`}>
+				<Button variant='light'>Return to the Home Page</Button>
+			</Link>
+		</>
 	)
 }
 
